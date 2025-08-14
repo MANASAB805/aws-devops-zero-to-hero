e018e90bf6cdf3a581800da568f85e254e267eb1
MANASAB805 <60311495+MANASAB805@users.noreply.github.com>
2025-08-14 17:06:59 +0800

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+# Pull the latest image
+docker pull manasab805/simple-python-flask-app
 
-# Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+# Run the container in detached mode
+docker run -d -p 5000:5000 --restart always manasab805/simple-python-flask-app
